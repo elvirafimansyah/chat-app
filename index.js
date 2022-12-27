fdceaff336e4804665aa9f519c349f1c1621e8c4
elvirafirmansyah <freddyfirmansyah25@gmail.com>
2022-12-27 22:57:43 +0700

    feat: userprofile

--- index.js
+++ index.js
@@ -20,21 +20,8 @@ io.on('connection', (socket) => {
   })
 
   socket.on("sendData", (room, id) => {
-    let split = room.split('--with--');
-    let unique = [...new Set(split)].sort((a, b) => (a < b ? -1 : 1)); 
-    let updatedRoomName = `${unique[0]}--with--${unique[1]}`;
-    Array.from(socket.rooms)
-      .filter(it => it !== id)
-      .forEach(id => {
-        socket.leave(id);
-      });
-    socket.join(room);
+    socket.userroom = room;
     socket.userid = id;
-    users.push(room);
-    socket.join(updatedRoomName);
-
-    socket.to(room).emit("wow", console.log(hello))
-
     socket.emit("sendData", {room: socket.room, id: socket.userid});
   });
   
