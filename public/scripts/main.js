393bf8639fb1fbae30380f7ea88f227d8a6ec8c5
elvirafirmansyah <freddyfirmansyah25@gmail.com>
2023-01-03 20:24:57 +0700

    fix: user profile

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -295,7 +295,7 @@ function saveAllMessage() {
     let allMessage = document.createElement("li");
     allMessage.classList.add("chat_list");
     allMessage.innerHTML = `
-      <div class="flex justify-between items-center " id="container_message"">
+      <div class="flex justify-between items-center " id="container_message" >
         <div class="flex items-center" data-id="${data.id}">
           <img src="${data.image}" class="w-12 h-12 mr-3 rounded-full"> <div>
             <div class="flex items-center ">
@@ -304,20 +304,23 @@ function saveAllMessage() {
               &nbsp;
               <span>${data.info_time}</span>
             </div>
-            <p class="bg-slate-200 rounded-br-3xl rounded-tr-3xl rounded-bl-xl p-2" id="message">${data.message}</p>
+            <p class="bg-slate-200 rounded-br-3xl rounded-tr-3xl rounded-bl-xl p-2" id="message" contenteditable="false">${data.message}</p>
           </div> 
         <div>
-        <button type="button"
-          class="p-2.5 text-sm font-medium text-white bg-red-500 rounded-lg border border-red-600 hover:bg-red-700 focus:outline-none hidden absolute right-2" title="delete message"
-          id="deleteBtn">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash"
-            viewBox="0 0 16 16">
-            <path
-              d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-            <path fill-rule="evenodd"
-              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-          </svg>
-        </button>
+        <div class="absolute right-2">
+          <button type="button"
+            class="p-2.5 text-sm font-medium text-white bg-red-500 rounded-lg border border-red-600 hover:bg-red-700 focus:outline-none hidden " title="delete message"
+            id="deleteBtn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash"
+              viewBox="0 0 16 16">
+              <path
+                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+              <path fill-rule="evenodd"
+                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+            </svg>
+          </button>
+          
+        </div>
       </div>
     `;  
 
@@ -328,14 +331,12 @@ function saveAllMessage() {
     containerMessage.addEventListener("mouseover",  () => {
       if(data.name === localStorage.getItem("name")) {
         deleteBtn.classList.remove("hidden");
-
       } 
     })
 
     containerMessage.addEventListener("mouseout", () => {
       deleteBtn.classList.add("hidden");
     })
-
 
     // Delete Function
     deleteBtn.addEventListener("click", (index) => {
