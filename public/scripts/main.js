60a1cc084f6cf5581e7cd9b23ad50bd00070cd13
elvirafirmansyah <freddyfirmansyah25@gmail.com>
2023-01-09 21:21:04 +0700

    feat: detect device

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -199,7 +199,14 @@ function profileUser() {
     <div class="flex items-center">
       <img src="${profileData.image}" class="rounded-full w-10 h-10 mr-2 border border-teal-500" alt="logo">&nbsp;
       <div>
-        <p class="self-center whitespace-nowrap text-lg font-medium  inline-block whitespace-pre-line">${profileData.name} </p>
+        <div class="flex items-center">
+          <p class="self-center whitespace-nowrap text-lg font-medium  inline-block whitespace-pre-line">${profileData.name}</p>&nbsp;${windowsUser ? `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#16a34a" class="bi bi-laptop" viewBox="0 0 16 16">
+            <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z"/>
+                </svg>` :  `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#16a34a" class="bi bi-tablet-landscape" viewBox="0 0 16 16">
+                <path d="M1 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4zm-1 8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8z"/>
+                <path d="M14 8a1 1 0 1 0-2 0 1 1 0 0 0 2 0z"/>
+              </svg>`}
+        </div>
         <p class="text-xs text-slate-800 id_text hover:cursor-pointer break-all" title="id">${profileData.id}</p>
       </div>
     </div>
