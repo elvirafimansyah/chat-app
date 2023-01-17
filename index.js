912036c297a8d11204a248d27a62e4099d6d5196
elvirafirmansyah <freddyfirmansyah25@gmail.com>
2023-01-17 20:20:47 +0700

    fix(5): socket io is not defined

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "*",
+    // methods: ["GET", "POST"]
+  }
+})
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
